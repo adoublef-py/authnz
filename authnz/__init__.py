ad2d657a2cd60a2f5d33ac362e06f41b0d3a168c
Kristopher Rahim <kristopherab@gmail.com>
2023-05-06 10:40:39 +0100

    test credentials domain type

--- authnz/__init__.py
+++ authnz/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 from uuid import UUID, uuid4
+from bcrypt import checkpw
 
-from validate import hash_password, parse_email, ValidationError
+from validate import compare_password, hash_password, parse_email, ValidationError
 
 @dataclass
 class User:
@@ -18,16 +19,20 @@ class Credentials:
     email: str
     password: bytes
 
+    def compare_password(self, password: str) -> bool:
+        """
+        Compare a password with a hashed password. An exception will be
+        raised if the passwords do not match.
+        """
+        return compare_password(password, self.password)
+
     def parse_credentials(email: str, password: str) -> Credentials:
         """
         Parse untrusted data into a Credentials object.
         """
-        try:
-            hash = hash_password(password)  # hash the password
-            email = parse_email(email)  # validate the email
-        except ValidationError as e:
-            raise e
-        
+        hash = hash_password(password)  # hash the password
+        email = parse_email(email)  # validate the email
+
         return Credentials(email, hash)
     
     def parse_dict(data: dict) -> Credentials:
--- tests/authnz/test_domain.py
+++ tests/authnz/test_domain.py
@@ -1,5 +1,5 @@
-from pytest import mark, raises
-from contextlib import nullcontext as does_not_raise
+from pytest import mark, raises, fixture
+from contextlib import contextmanager, nullcontext as does_not_raise
 from authnz import Credentials
 from validate import ValidationError
 
@@ -8,7 +8,22 @@ from validate import ValidationError
     ("test#mail.com", "this is a valid password", raises(ValidationError)),
     ("test#mail.com", "weak password", raises(ValidationError)),
 ])
-def test_parsing_credentials(email: str, password: str, expected: Exception):
+def test_parsing_credentials(email: str, password: str, expected: contextmanager):
     with expected:
         creds = Credentials.parse_credentials(email=email, password=password)
         assert creds is not None
+
+
+@fixture
+def valid_credentials():
+    email, password = ("test@mail.com", "this is a valid password")
+    return Credentials.parse_credentials(email, password)
+
+@mark.parametrize("password, expected", [
+    ("this is a valid password", does_not_raise()),
+    ("this is a wrong password", raises(ValidationError)),
+])
+def test_password_match(valid_credentials: Credentials, password: str, expected: contextmanager):
+    with expected: 
+        matched = valid_credentials.compare_password(password)
+        assert matched is True
